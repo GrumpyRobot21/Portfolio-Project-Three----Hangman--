52902f326476eab0286c45775dbda566b65f0b6d
Keithjmm <joenormal79@gmail.com>
2022-09-19 17:24:08 +0000

    add game play choice with error statement

--- run.py
+++ run.py
@@ -37,3 +37,17 @@ def start_intro():
        \\\__// 
     """)
     print(Colortext.BLUE + Colortext.BOLD + "Welcome to ye olde game of HANGMAN!!!!\n\nYou the player must carefully select letters in the vain hope of avoiding the gallows \nby guessing the word before it's too late!\n\nCan you cheat the hangmans noose in time?  Find out....if you dare!\n")
+
+    print(Colortext.BLUE + Colortext.BOLD + "Enter " +Colortext.GREEN + Colortext.BOLD + "'p' "+ Colortext.BLUE + Colortext.BOLD +  "to continue: ")
+
+    run = input('\n')
+    if run != ('p'):
+        print(Colortext.GREEN + Colortext.BOLD +"\n\nWRONG KEY!!! (I would go for the easy setting if I were you...). \n\nPress 'p' to continue: ")
+        time.sleep(3)
+        print('\033c', end='')  # clears the console - \033 is the ASCII escape character.
+        start_intro()
+    else:
+        print(Colortext.YELLOW + Colortext.BOLD + '\n\nGOOD LUCK!')
+        time.sleep(2)
+        print('\033c', end='')  # clears the console - \033 is the ASCII escape character.
+        game_rules()
