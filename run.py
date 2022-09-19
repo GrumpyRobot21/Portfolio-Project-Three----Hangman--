f172fcaecaf81bfa5263bf23b5dee45af7b75fd9
Keithjmm <joenormal79@gmail.com>
2022-09-19 17:21:14 +0000

    Class object for text color options

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random  # random choice method
+from gamewords import words  # custom gameword list
+import keyboard  # hotkey support for user interaction
+import string  # ascii alphabet function
+import time  # time sleep function
+import pyfiglet  # big letter graphics module
+
+
+class Colortext:
+    RED = '\033[0;31m'
+    GREEN = '\033[0;32m'
+    BLUE = '\033[0;34m'
+    YELLOW = '\033[1;33m'
+    BOLD = '\033[1m'
